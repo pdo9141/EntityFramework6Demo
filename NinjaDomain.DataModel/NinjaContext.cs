33680ac11edb89e3da4d4b6a29ae650f30ed0b0d
pdo9141 <pdo9141@gmail.com>
2016-09-07 20:56:05 -0700

    Add project files.

---
+++ NinjaDomain.DataModel/NinjaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using NinjaDomain.Classes;
+
+namespace NinjaDomain.DataModel
+{
+    public class NinjaContext : DbContext
+    {
+        public DbSet<Ninja> Ninjas { get; set; }
+
+        public DbSet<Clan> Clans { get; set; }
+
+        public DbSet<NinjaEquipment> Equipment { get; set; }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+1) Install EF Power Tools on VS 2015, you'll need this to check to see if EF will generate the
+desired DB schema from your classes
+http://thedatafarm.com/data-access/installing-ef-power-tools-into-vs2015/
